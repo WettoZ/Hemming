e4322b8348654a37f106bf0377a874cd8fe99e3f
Sergey Zaichkin <47739110+WettoZ@users.noreply.github.com>
2020-05-19 08:43:09 +0300

    Update settings.py

--- hem/settings.py
+++ hem/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/hem/apps/project/static/',
+    '/hem/static/',
     ]
 
 STATIC_URL = '/static/'
